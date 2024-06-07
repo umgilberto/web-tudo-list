6b17f7f54cd9102d01b9fa9ed1428e834408376f
gilberto19 <gilberto@smartinnovation.com.br>
2024-06-07 18:06:10 -0300

    feat: adding hidden password action

--- src/components/CustomComponents/InpitForm/index.tsx
+++ src/components/CustomComponents/InpitForm/index.tsx
@@ -7,22 +7,48 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { EyeClosedIcon, EyeOpenIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 import { Control, FieldValue } from "react-hook-form";
 
-interface Props {
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   name: string;
   label: string;
   control: Control<FieldValue<any>>;
-  placeholder?: string;
   description?: string;
 }
+
+const CustomInput = (props: any) => {
+  const [show, setShow] = useState(false);
+
+  const handleShowPassword = () => setShow(!show);
+
+  const isPassField = props.type === "password";
+  return (
+    <div className="relative inline-block w-full">
+      {isPassField &&
+        (show ? (
+          <EyeOpenIcon
+            onClick={handleShowPassword}
+            className="absolute right-2 top-1/4 cursor-pointer"
+          />
+        ) : (
+          <EyeClosedIcon
+            onClick={handleShowPassword}
+            className="absolute right-2 top-1/4 cursor-pointer"
+          />
+        ))}
+      <Input {...props} type={isPassField ? show ? "text" : "password" : "text"} />
+    </div>
+  );
+};
 
 export const InputForm = ({
   control,
   label,
   name,
   description,
-  placeholder,
+  ...rest
 }: Props) => {
   return (
     <FormField
@@ -32,7 +58,7 @@ export const InputForm = ({
         <FormItem>
           <FormLabel>{label}</FormLabel>
           <FormControl>
-            <Input placeholder={placeholder} {...field} />
+            <CustomInput {...rest} {...field} />
           </FormControl>
           <FormDescription>{description}</FormDescription>
           <FormMessage />
